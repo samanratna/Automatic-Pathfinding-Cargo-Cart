2cabeaba528c6276c11e34f0af2c67753ea81e8e
samanratna <samanratna2018@gmail.com>
2025-12-01 11:24:23 -0500

    add bluetooth code, but need to test

---
+++ apcc/src/distance_sensor.h
@@ -0,0 +1,18 @@
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+
+#include <Arduino.h>
+
+// === Ultrasonic sensor pin definitions ===
+#define US1_TRIG 5
+#define US1_ECHO 4
+
+// === Constants ===
+#define OBSTACLE_THRESHOLD_CM 30
+
+// === Function declarations ===
+void ultrasonic_setup(void);
+long getDistanceCM(int trigPin, int echoPin);
+long getDistanceForLineFollowing();
+
+#endif
