83c81dd07c6486d718cc13b757ac72940eec6570
samanratna <samanratna2018@gmail.com>
2025-12-05 10:48:02 -0500

    full code updated

--- apcc/src/distance_sensor.cpp
+++ apcc/src/distance_sensor.cpp
@@ -22,22 +22,22 @@ long getDistanceCM(int trigPin, int echoPin) {
 
 long getDistanceForLineFollowing() {
   
-  // uint8_t cumulation_count = 0;
-  // long distance_sum = 0;
+  uint8_t cumulation_count = 0;
+  long distance_sum = 0;
 
-  // for (int i = 0; i < 5; i++)
-  // {
-  //   long raw_distance = getDistanceCM(US1_TRIG, US1_ECHO);
-  //   if (raw_distance >= 0)
-  //   {
-  //     cumulation_count = cumulation_count + 1;
-  //     distance_sum = distance_sum + raw_distance;
-  //   }
-  // }
+  for (int i = 0; i < 5; i++)
+  {
+    long raw_distance = getDistanceCM(US1_TRIG, US1_ECHO);
+    if (raw_distance >= 0)
+    {
+      cumulation_count = cumulation_count + 1;
+      distance_sum = distance_sum + raw_distance;
+    }
+  }
 
-  // long average_distance = distance_sum / cumulation_count;
-  // return average_distance;
+  long average_distance = distance_sum / cumulation_count;
+  return average_distance;
 
-  long raw_distance = getDistanceCM(US1_TRIG, US1_ECHO);
-  return raw_distance;
+  // long raw_distance = getDistanceCM(US1_TRIG, US1_ECHO);
+  // return raw_distance;
 }
